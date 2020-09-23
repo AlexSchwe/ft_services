23982857de7b8acddff4f795dde21b24e023249d
AlexSchwe <alexandre.schwerer@hec.edu>
2020-09-23 20:07:19 +0200

    last before corrections

--- setup.sh
+++ setup.sh
@@ -1,72 +1,58 @@
-
-function set_goinfre()
-{
-	if [ -d "/goinfre" ]; then
-		# Ensure USER variabe is set
-		[ -z "${USER}" ] && export USER=`whoami`
-
-		mkdir -p /goinfre/$USER
-
-		# Set the minikube directory in /goinfre
-		export MINIKUBE_HOME="/goinfre/$USER"
-	fi
-}
-function set_minikube()
-{
-	if minikube &> /dev/null
-	then
-		echo -ne "\033[1;33m+>\033[0;33m Minikube check for upgrade ... \n"
-		if brew upgrade minikube &> /dev/null
-		then
-			echo -ne "\033[1;32m+>\033[0;33m Minikube updated ! \n"
-		else
-			echo -ne "\033[1;31m+>\033[0;33m Error... During minikube update. \n"
-			exit 1
-		fi
-	else
-		echo -ne "\033[1;31m+>\033[0;33m Minikube installation ...\n"
-		if brew install minikube &> /dev/null
-		then
-			echo -ne "\033[1;32m+>\033[0;33m Minikube installed ! \n"
-		else
-			echo -ne "\033[1;31m+>\033[0;33m Error... During minikube installation. \n"
-		fi
-	fi
-}
-
-#set_goinfre
-##set_minikube
-
 ADDONS=("metrics-server" "dashboard" "metallb" "default-storageclass" "storage-provisioner")
-UNITS=("nginx")
+#UNITS=("nginx" "ftps" "mysql" "wordpress" "phpmyadmin" "influxdb" "grafana")
+UNITS=("nginx" "ftps" "mysql" "wordpress" "phpmyadmin" "influxdb" "grafana")
 
 function launch_minikube()
 {
 	minikube delete
-	minikube start --vm-driver=virtualbox --memory 2000
+
+	if [ `uname -s` = 'Linux' ]
+	then
+		#VM settingd
+		VMDRIVER="docker"
+		VMCORE=2
+		MEMORY=5000
+	else
+		#Personnal settings: feel free to change
+		VMDRIVER="virtualbox"
+		VMCORE=2
+		MEMORY=2000
+	fi
+
+	#params are to be set according to your hardware
+	minikube start --vm-driver=$VMDRIVER --cpus=$VMCORE  --memory=$MEMORY
+#	minikube start --vm-driver=docker
 
 	for ADDON in ${ADDONS[@]}; do
 		minikube addons enable "${ADDON}"
 		done
 
+	#Installing metallb
 	kubectl apply -f metallb.yaml
 	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 	kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
 	export MINIKUBE_IP=$(minikube ip)
 }
 
 function build_services()
 {
 	eval $(minikube docker-env)
+
+	#Build the telegraf image inside each every container will be ran
 	docker build -t telegraf srcs/telegraf/
+
+	#Build every unit
 	for UNIT in ${UNITS[@]}; do
 		docker build -t "${UNIT}" srcs/${UNIT}
+		#pass minikube ip when needed as ENV variable
 		sed 's@$(MINIKUBE_IP)@'$MINIKUBE_IP'@g' srcs/${UNIT}/${UNIT}.yaml > srcs/${UNIT}/${UNIT}_service.yaml
 		kubectl apply -f srcs/${UNIT}/${UNIT}_service.yaml
+		#destroy the yaml file with Minikube_ip
 		rm srcs/${UNIT}/${UNIT}_service.yaml
 		done
 }
 
 launch_minikube
 build_services
-#screen -dmS "minikube dashboard"
+screen -dmS "minikube dashboard"
--- srcs/influxdb/script.sh
+++
@@ -1,3 +0,0 @@
-#Starts server in the background
-influxd run -config /etc/influxdb.conf > influx.log 2>&1 &
-tail -f /dev/null
